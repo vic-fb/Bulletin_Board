c6d1b0f59b067d5694a61bb6be0e755a728e3d06
Lauren Goldstein <laurengolds@gmail.com>
2022-10-26 10:17:16 +0200

    set up React Router and fixed package.json files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import HomeView from './views/HomeView';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+          <Route path="/" element={<HomeView />} />
+          {/* <Route path="about" element={<AboutView userCount={users.length} />} />
+          <Route path="users" element={<UsersView users={users} />} />
+          <Route path="users/:id" element={<UserProfileView users={users} />} />
+          <Route path="add-user" element={<AddUserView addUserCb={name => addUser(name)} />} />
+          <Route path="*" element={<Error404View />} /> */}
+      </Routes>
+      
+
+
     </div>
   );
 }
---
+++ client/src/views/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function HomeView() {
+    return (
+        <div className="HomeView">
+            <h2>Welcome to Bulletin Board!</h2>
+        </div>
+    );
+}
+
+export default HomeView;
