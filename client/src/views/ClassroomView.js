373181358759ec0efd4b06f6e2bc3cb6a824f96a
Lauren Goldstein <laurengolds@gmail.com>
2022-10-26 16:30:05 +0200

    ClassroomView shows projects of that classroom only

--- client/src/views/ClassroomView.js
+++ client/src/views/ClassroomView.js
@@ -5,14 +5,14 @@ import { Link, useParams } from 'react-router-dom';
 function ClassroomView(props) {
     let { id } = useParams();
     let classroom = props.classrooms.find(c => c.id === Number(id));
-
+    let projects = props.studentProjects.filter(p => p.classroom_id === Number(id));
 
     return (
         <div className="ClassroomView">
             <h2>Welcome to the Bulletin Board for {classroom.classroom_name}!</h2>
             <div>
                 {
-                    props.studentProjects.map((p) => (
+                    projects.map((p) => (
                         <h3 key={p.id} value={p.id}>
                             {p.title}
                             {/* <img url={p.image_url}/> */}
