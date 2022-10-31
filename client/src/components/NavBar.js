efd7b7f8df551edb361429e917102b4ec88c8c46
Lauren Goldstein <laurengolds@gmail.com>
2022-10-31 12:16:27 +0100

    added Dropdown Menu component to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,27 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import DropdownMenu from './DropdownMenu';
 
 
 function NavBar(props) {
-    let [options, setOptions] = useState([]);
+    // let [options, setOptions] = useState([]);
 
 
-    useEffect(() => {
-        let temp = generateOptions()
-        setOptions(temp);
-        props.getOptionsCb(options);
-    }, [props.classrooms]); // call whenever classrooms changes
+    // useEffect(() => {
+    //     let temp = generateOptions()
+    //     setOptions(temp);
+    //     props.getOptionsCb(options);
+    // }, [props.classrooms]); // call whenever classrooms changes
 
-    /* had to create generateOptions() outside rendering statement b/c page was 
-    being drawn before classrooms was loaded, so had to create options state and 
-    useEffect function to make sure data was available before page was rendered*/
-    function generateOptions() {
-        return props.classrooms.map((c) => (
-            <li><Link className="dropdown-item" to={`/classrooms/${c.id}`} key={c.id} value={c.id}>
-                {c.classroom_name} </Link>
-            </li>
-        ))
-    }
+    // /* had to create generateOptions() outside rendering statement b/c page was 
+    // being drawn before classrooms was loaded, so had to create options state and 
+    // useEffect function to make sure data was available before page was rendered*/
+    // function generateOptions() {
+    //     return props.classrooms.map((c) => (
+    //         <li><Link className="dropdown-item" to={`/classrooms/${c.id}`} key={c.id} value={c.id}>
+    //             {c.classroom_name} </Link>
+    //         </li>
+    //     ))
+    // }
   
     return (
         <nav className="navbar navbar-expand-lg bg-light">
@@ -37,9 +38,8 @@ function NavBar(props) {
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Classrooms
                             </a>
-                            <ul className="dropdown-menu">
-                                {options}
-                            </ul>
+                            <DropdownMenu classrooms={props.classrooms}
+                                getOptionsCb={props.getOptionsCb} />
                         </li>
                     </ul>
                 </div>
--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -5,26 +5,7 @@ import DropdownMenu from '../components/DropdownMenu';
 
 
 function HomeView(props) {
-//     let [options, setOptions] = useState([]);
-//     const navigate = useNavigate();
-    
 
-//     useEffect(() => {
-//         let temp = generateOptions()
-//         setOptions(temp);
-//         props.getOptionsCb(options);
-//       }, [props.classrooms]); // call whenever classrooms changes
-
-// // /* had to create generateOptions() outside rendering statement b/c page was 
-// // being drawn before classrooms was loaded, so had to create options state and 
-// // useEffect function to make sure data was available before page was rendered*/
-//    function generateOptions() {  
-//         return props.classrooms.map((c) => (
-//             <li><Link className="dropdown-item" to={`/classrooms/${c.id}`} key={c.id} value={c.id}>
-//                 {c.classroom_name} </Link>
-//             </li>
-//         ))
-//     }
     console.log(props)
     return (
         <div className="HomeView">
