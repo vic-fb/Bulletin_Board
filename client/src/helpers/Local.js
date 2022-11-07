e06024cfeedc439dc02a62b441b21d5a7cb7077b
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-07 10:51:48 +0100

    feat(login): Add local storage

---
+++ client/src/helpers/Local.js
@@ -0,0 +1,46 @@
+/**
+ * All localStorage implementation is here
+ * */
+
+class Local {
+  static saveUserInfo(token, user) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  static removeUserInfo() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  static getToken() {
+    return (localStorage.getItem('token') || '');
+  }
+
+  static getUser() {
+    const userjson = localStorage.getItem('user');
+    return userjson ? JSON.parse(userjson) : null;
+  }
+
+  static getUserId() {
+    const userjson = localStorage.getItem('user');
+    if (!userjson) {
+      return '';
+    }
+
+    const user = JSON.parse(userjson);
+    return user.id;
+  }
+
+  static getUsername() {
+    const userjson = localStorage.getItem('user');
+    if (!userjson) {
+      return '';
+    }
+
+    const user = JSON.parse(userjson);
+    return user.username;
+  }
+}
+
+export default Local;
