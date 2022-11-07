f8749eb5bc9d19ce6c5a9846d406fbed9370b50b
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-07 10:41:49 +0100

    feat(database): Add email and password to users table

--- model/init_db.sql
+++ model/init_db.sql
@@ -6,13 +6,14 @@ SET foreign_key_checks = 1;
 
 CREATE TABLE users(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100) NULL,
-    last_name VARCHAR(100) NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NULL,
+    password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL,
-    classroom_id INT UNSIGNED NOT NULL,
+    classroom_id INT UNSIGNED NULL,
     primary key (id)
 );
-
 
 CREATE TABLE classrooms(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -45,8 +46,8 @@ ALTER TABLE
 INSERT INTO classrooms (classroom_name, assignment_title, assignment_desc)
     VALUES ("Mr. Goleman's 5th Grade Classroom", 'Ancient Egpyt Project', 'Students will research a topic of their choice and create a project to show their findings.');
 
-INSERT INTO users (first_name, last_name, role, classroom_id)
-    VALUES ('Michael', 'Goleman', 'teacher', 1), ('Sara', 'Norgaard', 'student', 1), ('Stu', 'Selwig', 'student', 3);
+INSERT INTO users (first_name, last_name, email, password, role, classroom_id)
+    VALUES ('Michael', 'Goleman', 'mrgoleman@test.ed', 'password', 'teacher', 1), ('Sara', 'Norgaard', 'student@test.ed', 'random','student', 1), ('Stu', 'Selwig','student@test.ed', 'random', 'student', 3);
 
 INSERT INTO student_projects (user_id, title, description, image_url, project_url, classroom_id)
     VALUES (2, 'Mummies', 'I researched how mummies were made in Ancient Egypt', 'https://media.istockphoto.com/photos/close-up-of-ancient-mummy-casket-picture-id139982295?k=20&m=139982295&s=612x612&w=0&h=pfmGRS8icf9lncFeljSyWJdAYJDIL-8eiLA3aqJpR60=', 'https://www.bbc.com/news/world-middle-east-56926005', 1),
