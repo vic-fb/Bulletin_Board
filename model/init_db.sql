66cc1360ec387b2c1ec3f95f7ca69fd6b08335a0
Vicky Fernandez Busch <mvfbusch@gmail.com>
2022-11-08 14:37:53 +0100

    feat(database): Make password not required

--- model/init_db.sql
+++ model/init_db.sql
@@ -9,7 +9,7 @@ CREATE TABLE users(
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NULL,
     role VARCHAR(50) NOT NULL,
     classroom_id INT UNSIGNED NULL,
     primary key (id)
